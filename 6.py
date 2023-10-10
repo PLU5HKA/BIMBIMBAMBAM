d89a9cf362501b343c6e3598c08512455495b0da
PLU5HKA <pluska55520@gmail.com>
2023-10-10 23:27:03 +0700

    6 - fifth central moment?

--- 6.py
+++ 6.py
@@ -1,23 +1,19 @@
 import math
 
-
 def compute_statistics(arr):
     minimum = min(arr)
     maximum = max(arr)
     average = sum(arr) / len(arr)
-
     squared_diff_sum = sum((x - average) ** 2 for x in arr)
     standard_deviation = math.sqrt(squared_diff_sum / len(arr))
-
     fifth_central_moment = sum((x - average) ** 5 for x in arr) / len(arr)
-
     return minimum, maximum, average, standard_deviation, fifth_central_moment
 
 
 # Пример использования
-array = [1, 2, 3, 4, 5]
+array = [2, 4, 6, 8, 10]
 minimum, maximum, average, standard_deviation, fifth_central_moment = compute_statistics(array)
-
+print(array)
 print("Минимум:", minimum)
 print("Максимум:", maximum)
 print("Среднее значение:", average)
