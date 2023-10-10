cf0b17956e0b733f9669b39523cae0ff4ecfaae7
PLU5HKA <pluska55520@gmail.com>
2023-10-10 22:46:20 +0700

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,14 @@
+import sympy as sp
+from scipy import integrate
+# Аналитически
+x = sp.Symbol('x')
+f = str(input('f(x)='))
+a = int(input("a="))
+b = int(input("b="))
+print(sp.integrate(f,(x,a,b)))
+# Численно
+f2 = str(input('f2(x)='))
+a2 = float(input("a2="))
+b2 = float(input("b2="))
+integral, error = integrate.quad(lambda x:eval(f2), a2, b2)
+print(integral)
