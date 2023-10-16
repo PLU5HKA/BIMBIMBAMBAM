2beffa12c4df2228e68ee79dcfa02f80ba7694b1
PLU5HKA <pluska55520@gmail.com>
2023-10-16 15:13:40 +0700

    6

--- 6.py
+++ 6.py
@@ -1,21 +1,22 @@
 import math
+import scipy
 
-def compute_statistics(arr):
+def statistics(arr):
     minimum = min(arr)
     maximum = max(arr)
     average = sum(arr) / len(arr)
     squared_diff_sum = sum((x - average) ** 2 for x in arr)
     standard_deviation = math.sqrt(squared_diff_sum / len(arr))
-    fifth_central_moment = sum((x - average) ** 5 for x in arr) / len(arr)
-    return minimum, maximum, average, standard_deviation, fifth_central_moment
+    fifth_central_moment = scipy.stats.moment(arr, 5)
+    averageloc = (minimum + maximum + average + squared_diff_sum + fifth_central_moment)/5
+    return minimum, maximum, average, standard_deviation, fifth_central_moment, averageloc
 
-
-# Пример использования
-array = [2, 4, 6, 8, 10]
-minimum, maximum, average, standard_deviation, fifth_central_moment = compute_statistics(array)
+array = [1, 4, 5, 9, 11]
+minimum, maximum, average, standard_deviation, fifth_central_moment, averageloc = statistics(array)
 print(array)
 print("Минимум:", minimum)
 print("Максимум:", maximum)
 print("Среднее значение:", average)
 print("Среднеквадратическое отклонение:", standard_deviation)
 print("Пятый центральный момент:", fifth_central_moment)
+print("Среднее из вышеуказанных:", averageloc)
