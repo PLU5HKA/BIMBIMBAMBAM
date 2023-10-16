67e7a409f32f7c3c2f53a2e857b2e0ffb6a51d81
PLU5HKA <pluska55520@gmail.com>
2023-10-16 16:39:28 +0700

    5

--- 7.py
+++ 7.py
@@ -1,15 +1,16 @@
 import cmath
 R = [22, 3, 9, 0, 111, 4]
-R.sort()
-print(R)
 C = [6+5j, 1+5j, 4+2j, -4+1j, 8-4j]
-C.sort(key=lambda x: abs(x))
-print(C)
-C.sort(key=lambda x: x.real, reverse=True)
-print(C)
 S = ['abvg', 'bimbimbambam', 'abvgeezh', 'bim', 'bam', 'qwerty']
-S.sort(key=len)
-print(S)
-print(tuple(sorted(S)))
 A = [[1, 2, 3], [1, 2], [1, 2, 3, 4, 5], [1, 2, 3, 4]]
-print(tuple(sorted(A, key=len)))
+print(f'{R}\n{C}\n{S}\n{A}')
+R.sort()
+print('Сортировка по возрастанию',R)
+C.sort(key=lambda x: abs(x))
+print('массив комплексных чисел по модулю',C)
+C.sort(key=lambda x: x.real, reverse=True)
+print('массив комплексных чисел по убыванию действительной части',C)
+S.sort(key=len)
+print('список строк по длине',S)
+print('кортеж строк в лексикографическом порядке',tuple(sorted(S)))
+print('кортеж списков по возрастанию длины',tuple(sorted(A, key=len)))
